f54dd91f169f9610bc9a0565abd80bb6ceaa0426
jaystar13 <31879736+jaystar13@users.noreply.github.com>
2025-10-08 04:48:55 +0000

    Deploying to gh-pages from @ jaystar13/jaystar13.github.io@8529bedbdfc0eb784e7500c8fa5678a61cba35f1 🚀

---
+++ assets/js/search-data.js
@@ -0,0 +1,158 @@
+// get the ninja-keys element
+const ninja = document.querySelector('ninja-keys');
+
+// add the home and posts menu items
+ninja.data = [{
+    id: "nav-about",
+    title: "about",
+    section: "Navigation",
+    handler: () => {
+      window.location.href = "/";
+    },
+  },{id: "nav-blog",
+          title: "blog",
+          description: "",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/blog/";
+          },
+        },{id: "nav-applications",
+          title: "applications",
+          description: "실생활에 유용한 앱을 개발합니다.",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/applications/";
+          },
+        },{id: "post-dynamic-form-submit",
+        
+          title: "Dynamic Form Submit",
+        
+        description: "스프링 부트와 타임리프를 이용한 동적인 폼 요소 제출",
+        section: "Posts",
+        handler: () => {
+          
+            window.location.href = "/blog/2024/dynamic-form-submit/";
+          
+        },
+      },{id: "post-dynamic-form-search-refactoring",
+        
+          title: "Dynamic Form Search - Refactoring",
+        
+        description: "스프링 부트와 타임리프를 이용한 동적인 폼 요소 조회 - 리팩토링",
+        section: "Posts",
+        handler: () => {
+          
+            window.location.href = "/blog/2024/dynamic-form-refactoring/";
+          
+        },
+      },{id: "post-dynamic-form-search-with-spring-boot-thymeleaf",
+        
+          title: "Dynamic Form Search with Spring Boot + Thymeleaf",
+        
+        description: "스프링 부트와 타임리프를 이용한 동적인 폼 요소 조회",
+        section: "Posts",
+        handler: () => {
+          
+            window.location.href = "/blog/2024/dynamic-form/";
+          
+        },
+      },{id: "applications-청약계산소",
+          title: '청약계산소',
+          description: "내 청약 인정회차를 한눈에!",
+          section: "Applications",handler: () => {
+              window.location.href = "/applications/chungyak-box/";
+            },},{id: "books-the-godfather",
+          title: 'The Godfather',
+          description: "",
+          section: "Books",handler: () => {
+              window.location.href = "/books/the_godfather/";
+            },},{id: "news-a-simple-inline-announcement",
+          title: 'A simple inline announcement.',
+          description: "",
+          section: "News",},{id: "news-a-long-announcement-with-details",
+          title: 'A long announcement with details',
+          description: "",
+          section: "News",handler: () => {
+              window.location.href = "/news/announcement_2/";
+            },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
+          title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
+          description: "",
+          section: "News",},{id: "projects-project-1",
+          title: 'project 1',
+          description: "with background image",
+          section: "Projects",handler: () => {
+              window.location.href = "/projects/1_project/";
+            },},{id: "projects-project-2",
+          title: 'project 2',
+          description: "a project with a background image and giscus comments",
+          section: "Projects",handler: () => {
+              window.location.href = "/projects/2_project/";
+            },},{id: "projects-project-3-with-very-long-name",
+          title: 'project 3 with very long name',
+          description: "a project that redirects to another website",
+          section: "Projects",handler: () => {
+              window.location.href = "/projects/3_project/";
+            },},{id: "projects-project-4",
+          title: 'project 4',
+          description: "another without an image",
+          section: "Projects",handler: () => {
+              window.location.href = "/projects/4_project/";
+            },},{id: "projects-project-5",
+          title: 'project 5',
+          description: "a project with a background image",
+          section: "Projects",handler: () => {
+              window.location.href = "/projects/5_project/";
+            },},{id: "projects-project-6",
+          title: 'project 6',
+          description: "a project with no image",
+          section: "Projects",handler: () => {
+              window.location.href = "/projects/6_project/";
+            },},{id: "projects-project-7",
+          title: 'project 7',
+          description: "with background image",
+          section: "Projects",handler: () => {
+              window.location.href = "/projects/7_project/";
+            },},{id: "projects-project-8",
+          title: 'project 8',
+          description: "an other project with a background image and giscus comments",
+          section: "Projects",handler: () => {
+              window.location.href = "/projects/8_project/";
+            },},{id: "projects-project-9",
+          title: 'project 9',
+          description: "another project with an image 🎉",
+          section: "Projects",handler: () => {
+              window.location.href = "/projects/9_project/";
+            },},{
+        id: 'social-email',
+        title: 'email',
+        section: 'Socials',
+        handler: () => {
+          window.open("mailto:%72%61%73%63%63%6F%6C%69%69@%67%6D%61%69%6C.%63%6F%6D", "_blank");
+        },
+      },{
+      id: 'light-theme',
+      title: 'Change theme to light',
+      description: 'Change the theme of the site to Light',
+      section: 'Theme',
+      handler: () => {
+        setThemeSetting("light");
+      },
+    },
+    {
+      id: 'dark-theme',
+      title: 'Change theme to dark',
+      description: 'Change the theme of the site to Dark',
+      section: 'Theme',
+      handler: () => {
+        setThemeSetting("dark");
+      },
+    },
+    {
+      id: 'system-theme',
+      title: 'Use system default theme',
+      description: 'Change the theme of the site to System Default',
+      section: 'Theme',
+      handler: () => {
+        setThemeSetting("system");
+      },
+    },];
